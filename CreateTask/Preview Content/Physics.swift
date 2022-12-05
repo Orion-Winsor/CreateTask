2045e3153a3915e0a7a6e456b9a6293cbfc9bb33
Orion-Winsor <111298704+Orion-Winsor@users.noreply.github.com>
2022-12-05 07:28:24 -0700

    added several methods from the IOS game to get blocks working

--- CreateTask/Preview Content/Physics.swift
+++ CreateTask/Preview Content/Physics.swift
@@ -11,15 +11,24 @@ import SpriteKit
 class Physics : SKScene, SKPhysicsContactDelegate
 {
     private var colorMask : Int = 0b0000
+    private var gameBlocks : [SKSpriteNode] = []
+
+    override func didMove(to view: SKView)
+    {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
+        loadSand()
+    }
     
-    override func touchesBegan(_ touches : Set<UITouch>, with event : UIEvent?) -> Void
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
         guard let touch = touches.first
         else{ return }
         
         let location = touch.location(in: self)
-        
-        func loadSand() -> Void
+    }
+    
+    func loadSand() -> Void
         {
             for _ in 0 ..< 160
             {
@@ -27,8 +36,19 @@ class Physics : SKScene, SKPhysicsContactDelegate
                 var width = 50
                 var height = 50
                 
+                if height != 50 || width != 50
+                {
+                    width = 50
+                    height = 50
+                }
+                
                 let node: SKSpriteNode
                 node = SKSpriteNode(color: currentColor, size: CGSize( width: width, height: height))
+                
+                node.position = location
+                node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: width, height: height))
+                node.physicsBody?.contactTestBitMask = UInt32(colorMask)
+                gameBlocks.append(node)
             }
         }
 
@@ -42,6 +62,21 @@ class Physics : SKScene, SKPhysicsContactDelegate
             return colors[randomIndex]
         }
 
+    private func collision(_ nodeOne : SKNode, and nodeTwo: SKNode) -> Void
+    {
+        if(nodeOne.physicsBody?.contactTestBitMask ==
+           nodeTwo.physicsBody?.contactTestBitMask)
+        {
+            //unneded
+        }
+    }
+    
+    func didBegin(_ contact : SKPhysicsContact) -> Void
+    {
+        guard let first = contact.bodyA.node else {return}
+        guard let second = contact.bodyB.node else { return }
+        
+        collision(first, and: second)
     }
     
     
